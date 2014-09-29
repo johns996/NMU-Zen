030192b4c17051383786a339724420439a76b8e4
Eric Johnson <ericjohn@nmu.edu>
2014-09-29 09:52:40 -0400

    rename

---
+++ theme-includes/branding.php
@@ -0,0 +1,6 @@
+<?php
+//nmu-zen
+?>
+
+<div class="branding-one allcaps">Northern</div>
+<div class="branding-two allcaps">Michigan University</div>
---
+++ theme-includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+//nmu-zen
+?>
+
+<div class="footer-main">
+<img src="/responsivenmu/sites/DrupalResponsiveNMU/files/UserFiles/footer.png" />
+<!--<h5>Northern Michigan University</h5>
+<p>1401 Presque Isle Ave. • Marquette, MI 49855-5301 • 906-227-1000</p>
+<p>© 2014 by the NMU Board of Trustees. NMU is an equal opportunity institution.</p>
+<p><a href="#">Admissions Questions</a> | <a href="#">Contact Us</a> | <a href="#">Privacy Statement</a> | <a href="#">Emergency Information</a></p> -->
+</div>
