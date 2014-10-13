5e52c2dea8d5dabd757f58174da792cfd6192d2d
Eric Johnson <ericjohn@nmu.edu>
2014-10-13 16:57:34 -0400

    jasons video stuff

--- js/hp-video/hp-video.js
+++ js/hp-video/hp-video.js
@@ -1,4 +1,19 @@
-$(document).ready(function($) {
+jQuery(document).ready(function($) {
+                var idleInterval = setInterval(timerIncrement, 60000); //1 minute intervals
+                $(this).mousemove(function(e) {
+                    idleTime = 0;
+                });
+                $(this).keypress(function(e) {
+                    idleTime = 0;
+                });
+
+                function timerIncrement() {
+                    idleTime = idleTime + 1
+                    if (idleTime > 14) {
+                        Video_back.pause();
+                    }
+                }
+
 		var Video_back = new video_background($("#fearless-video"), {
 				"position": "absolute", //Stick within the div
 				"z-index": "-1", //Behind everything
@@ -9,18 +24,13 @@ $(document).ready(function($) {
 				"mp4": "http://ericjohnpc.nmu.edu/responsivenmu/sites/DrupalResponsiveNMU/files/UserFiles/Silent-Fearless.mp4", //mp4 video link
 				"webm": "http://ericjohnpc.nmu.edu/responsivenmu/sites/DrupalResponsiveNMU/files/UserFiles/Silent-Fearless.webm", //Path to video webm format
 
-				//"youtube": "JtpUN4_mXhI",
-				//"start": 5,					//Start with 6 seconds offset (to pass the introduction in this case for example)
 				"video_ratio": 1.7777778, // width/height -> If none provided sizing of the video is set to adjust
 
 				"fallback_image": "testImage.png", //Fallback image path
 		});
-
-
 		$("#playYoutube").click(function() {
 				$("#fearless-video").empty();
 				$(".video-overlay").hide();
-				$("#togglePlay").show();
 				var Video_back = new video_background($("#fearless-video"), {
 						"position": "absolute", //Stick within the div
 						"z-index": "-1", //Behind everything
@@ -29,13 +39,10 @@ $(document).ready(function($) {
 						"autoplay": true, //Autoplay at start
 						"muted": false, //Not Muted
 						"youtube": "JtpUN4_mXhI",
-						//"start": 5,					//Start with 6 seconds offset (to pass the introduction in this case for example)
+                        "start": 0, //Start with X seconds offset
 						"video_ratio": 1.7777778, // width/height -> If none provided sizing of the video is set to adjust
 
 						"fallback_image": "testImage.png", //Fallback image path
 				});
-				$("#togglePlay").click(function() {
-						Video_back.toggle_play();
 				});
 		});
-});
