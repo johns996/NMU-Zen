416fbe95fa34196d9a380274c28653282cf49c3a
Josh Fridstrom <jdfstrom@gmail.com>
2014-10-13 15:13:27 -0400

    added hp columns and footer

--- theme-includes/footer.php
+++ theme-includes/footer.php
@@ -3,9 +3,22 @@
 ?>
 
 <div class="footer-main">
-<img src="/responsivenmu/sites/DrupalResponsiveNMU/files/UserFiles/footer.png" />
-<!--<h5>Northern Michigan University</h5>
-<p>1401 Presque Isle Ave. • Marquette, MI 49855-5301 • 906-227-1000</p>
-<p>© 2014 by the NMU Board of Trustees. NMU is an equal opportunity institution.</p>
-<p><a href="#">Admissions Questions</a> | <a href="#">Contact Us</a> | <a href="#">Privacy Statement</a> | <a href="#">Emergency Information</a></p> -->
+	<h1>Northern Michigan University</h1>
+	<p>1401 Presque Isle Ave.<span>&bull;</span>Marquette, MI 49855-5301<span>&bull;</span>906&ndash;227&ndash;1000<p>
+	<p>&copy; 2014 by the NMU Board of Trustees. NMU is an equal opportunity institution.</p>
+	<div class="reporting">
+		<a href="/budget-reporting">
+			<img style="width: 50px; height: 50px" src="images/budget.png"/>
+			<p>Budget and Performance Transparency Reporting</p>
+		</a>
+	</div>
+	<hr/>
+	<nav>
+		<ul>
+			<li><a href="/admissions">Admissions Questions</a></li>
+			<li><a href="/contactus">Contact Us</a></li>
+			<li><a href="/privacy">Privacy Statement</a></li>
+			<li><a href="/emergency">Emergency Information</a></li>
+		</ul>
+	</nav>
 </div>
