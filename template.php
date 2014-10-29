4e106215e15b5b75cfd722749dd73c2c8a119bb9
Josh Fridstrom <jdfstrom@gmail.com>
2014-10-29 16:59:15 -0400

    New styles for admissions homepage. Refactored cycle2 styles out to be reusable.

--- template.php
+++ template.php
@@ -78,6 +78,11 @@ function zen_nmu_preprocess_node(&$variables, $hook) {
 		//drupal_add_js(drupal_get_path('theme', 'zen_nmu') . '/js/homepage-v2.js');
 		//$js = drupal_add_js();
 		//$scripts = drupal_get_js($js);
+	//add a css file to the fearless_nmu_admissions
+	} else if($variables['type'] == 'fearless_nmu_admissions'){
+		drupal_add_css(drupal_get_path('theme', 'zen_nmu') . '/css/nmu-admissions.css');
+		$css = drupal_add_css();
+		$styles = drupal_get_css($css);
 	}
 }
 //
