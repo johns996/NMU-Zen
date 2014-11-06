1508690865bdecd13a0281ffd913a746492ef63b
Eric Johnson <ericjohn@nmu.edu>
2014-11-06 12:07:34 -0500

    footer finished

--- theme-includes/footer.php
+++ theme-includes/footer.php
@@ -3,21 +3,21 @@
 ?>
 
 <div class="footer-main">
-	<h1>Northern Michigan University</h1>
-	<p>1401 Presque Isle Ave.<span>&bull;</span>Marquette, MI 49855-5301<span>&bull;</span>906&ndash;227&ndash;1000<p>
-	<p>&copy; 2014 by the NMU Board of Trustees. NMU is an equal opportunity institution.</p>
+	<h4>Northern Michigan University</h4>
+	<p>1401 Presque Isle Ave. <span class="bullet">&bull;</span> Marquette, MI 49855-5301 <span class="bullet">&bull;</span> 906&ndash;227&ndash;1000<p>
+	<p>&copy; 2014 by the <a href="/board">NMU Board of Trustees</a>. NMU is an <a href="/equalop">equal opportunity institution</a>.</p>
 	<div class="reporting">
-		<a href="/budget-reporting">
-			<img style="width: 50px; height: 50px" src="/sites/all/themes/zen_nmu/images/budget.png"/>
-			<p>Budget and Performance Transparency Reporting</p>
+		<a href="/finance/transparency_reporting">
+			<img src="/sites/all/themes/zen_nmu/images/budget.png"/>
+			<p>Budget and Performance<br /> Transparency Reporting</p>
 		</a>
 	</div>
 	<hr/>
 	<nav>
 		<ul>
-			<li><a href="/admissions">Admissions Questions</a></li>
+			<li><a href="mailto:admiss@nmu.edu">Admissions Questions</a></li>
 			<li><a href="/contactus">Contact Us</a></li>
-			<li><a href="/privacy">Privacy Statement</a></li>
+			<li><a href="/communicationsandmarketing/PrivacyStatement">Privacy Statement</a></li>
 			<li><a href="/emergency">Emergency Information</a></li>
 		</ul>
 	</nav>
