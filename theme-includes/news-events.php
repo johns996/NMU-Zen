65a66f7183ad5a3a61e797e0ec335dc6481e8192
johns996 <eric.johnson.x@gmail.com>
2014-11-19 17:20:53 -0500

    new item for news and events block

---
+++ theme-includes/news-events.php
@@ -0,0 +1,16 @@
+<?php
+//nmu-zen
+?>
+
+<div class="news-events">
+	<div class="events-header">
+		<div class="events-label allcaps">News &<br /><span class="events-sub-label">Upcoming Events</span></div>
+	</div>
+	<div class="row events-section">
+		<?php
+			if(!$strHP) //make sure something was set in the block
+				$strHP = "NMU Homepage";   //Pick this from the cms_events_categories table
+			require_once('/htdocs/cmsphp/Admin/MiscInterfaces/HPEvents.php');
+		?>
+	</div>
+</div>
