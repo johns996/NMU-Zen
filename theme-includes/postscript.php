18f24093a60b1adfa39d573a088e55412db5f89e
Eric Johnson <ericjohn@nmu.edu>
2014-12-12 10:08:21 -0500

    revisions to internal page and button on the bottom of news and events

---
+++ theme-includes/postscript.php
@@ -0,0 +1,25 @@
+<div class="container">
+	<div class="postscript row">
+		<div class="gallery col-md-6">
+			<img src="/sites/all/themes/zen_nmu/images/photo-gallery.jpg">
+			<div class="gallery-text allcaps">
+				<span class="gallery-visit">Visit Our</span><br />
+				Photo Gallery
+				<div class="gallery-button allcaps">
+					NMU Photo Gallery
+				</div>
+			</div>
+		</div>
+
+		<div class="share col-md-6">
+			<img src="/sites/all/themes/zen_nmu/images/share-nmu.jpg">
+			<div class="share-text allcaps">
+				<span class="share-share">Share NMU</span><br />
+				Connect With Us
+				<div class="share-buttons allcaps">
+					<a href="#" class="share-facebook"></a> <a href="#" class="share-twitter"></a> <a href="#" class="share-instagram"></a> <a href="#" class="share-linkedin"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
