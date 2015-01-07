e7933ec8ea484fde320af417d7cc63ab5192f8e5
Chris Danik <cdanik@gmail.com>
2015-01-07 11:12:16 -0500

    Added check for NMU emergency alert message.

--- theme-includes/branding.php
+++ theme-includes/branding.php
@@ -11,11 +11,16 @@
 //alert-success -> green
 //alert-info -> blue
 
-echo '
-<div class="alert alert-info alert-dismissible alert-nmu" id="nmu-alert" role="alert">
-  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-  <strong>This is a preview of the upcoming website redesign.</strong>  <a href="http://goo.gl/forms/G9VVuhz1QL">Provide feedback here</a>.<br />
-  <strong>Note:</strong> This alert section would only be present on the page (and colored red) during campus emergencies.
-</div>
-';
+$alertMessageFile = "/htdocs/Webb/DynamicallyCreatedFiles/NMU/emergency_alert_msg.shtml";
+
+printR(filesize($alertMessageFile));
+
+if(filesize($alertMessageFile) >  0){
+	echo '
+		<div class="alert alert-danger alert-dismissible alert-nmu" id="nmu-alert" role="alert">
+		  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+		  '.file_get_contents($alertMessageFile).'
+		</div>
+		';
+}
 ?>
