6676cec2507cb3e55b2e740834491e1badf32ee6
Josh Fridstrom <jfridstr@nmu.edu>
2015-01-09 14:37:06 -0500

    Added links to calendar and news.

--- theme-includes/news-events.php
+++ theme-includes/news-events.php
@@ -16,7 +16,7 @@
 		</div>
 	</div>
 	<div class="events-footer row">
-		<a href="" class="events-link allcaps">Events Calendar</a>
-		<div class="news-link"><a href="">View Latest News</a></div>
+		<a href="/calendar" class="events-link allcaps">Events Calendar</a>
+		<div class="news-link"><a href="/communicationsandmarketing/news-releases">View Latest News</a></div>
 	</div>
 </div>
