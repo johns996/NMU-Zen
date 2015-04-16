3989428f77705bbb1207db44915e909e45c41b99
Eric Johnson <ericjohn@nmu.edu>
2015-04-16 09:47:14 -0400

    changed color of error message and copied in bootstrap styles for omega

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -14,10 +14,39 @@ $strURL = $strBaseURL.'/saml_login';
 Before access to any site is granted, a CMS training session must be completed first.  To request a training session and
 access to a site, please contact <a href="mailto:ericjohn@nmu.edu">Eric Johnson</a>.</p>
 
-<div style="background-color:#fccccc; margin:20px; padding:10px; border:1px dotted red; max-width:80%;">
-<h3>State Information Lost Error</h3>
-<p>Many Users have been reporting an error with the CMS log in process.  After entering their username and password an error screen with the message <em>State Information Lost</em> appears.  This error is specific to Internet Explorer 9 and how it handles SSL.  <strong>The only fix for the error at this point is using a different version of Internet Explorer (10 or 11) or using Firefox or Chrome</strong>.</p>
-</div>
+
+		<style>
+		.alert {
+			padding: 15px;
+			margin: 0 20px 20px 20px;
+			border: 1px solid transparent;
+			border-radius: 4px;
+		}
+		.alert-warning {
+			color: #8a6d3b;
+			background-color: #fcf8e3;
+			border-color: #faebcc;
+		}
+		.alert {
+			text-shadow: 0 1px 0 rgba(255,255,255,.2);
+			-webkit-box-shadow: inset 0 1px 0 rgba(255,255,255,.25),0 1px 2px rgba(0,0,0,.05);
+			box-shadow: inset 0 1px 0 rgba(255,255,255,.25),0 1px 2px rgba(0,0,0,.05);
+		}
+		.alert-warning {
+			background-image: -webkit-linear-gradient(top,#fcf8e3 0,#f8efc0 100%);
+			background-image: -o-linear-gradient(top,#fcf8e3 0,#f8efc0 100%);
+			background-image: -webkit-gradient(linear,left top,left bottom,from(#fcf8e3),to(#f8efc0));
+			background-image: linear-gradient(to bottom,#fcf8e3 0,#f8efc0 100%);
+			filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#fffcf8e3', endColorstr='#fff8efc0', GradientType=0);
+			background-repeat: repeat-x;
+			border-color: #f5e79e;
+		}
+		</style>
+
+		<div class="alert alert-warning" role="alert">
+		<h3>State Information Lost Error</h3>
+		<p>Many Users have been reporting an error with the CMS log in process.  After entering their username and password an error screen with the message <em>State Information Lost</em> appears.  This error is specific to Internet Explorer 9 and how it handles SSL.  <strong>The only fix for the error at this point is using a different version of Internet Explorer (10 or 11) or using Firefox or Chrome</strong>.</p>
+		</div>
 
 <div id="cms_login">
 	<strong>Authorized Users:</strong><br />
